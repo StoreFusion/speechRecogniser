e69c6c34e51f7347b99d20ff5ea553255ca49287
StoreFusion <tdur@protonmail.com>
2023-10-22 16:45:03 +0530

    [changes] to the continous recording and display features, basic

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEv
 let recognition = new SpeechRecognition();
 recognition.continuous = true;
 recognition.lang = 'en-US';
-recognition.interimResults = false;
+recognition.interimResults = true;
 recognition.maxAlternatives = 1;
 
 recognition.onresult = (event) => {
@@ -13,20 +13,34 @@ recognition.onresult = (event) => {
     console.log(speech);
 }
 
-
 recognition.onspeechend = function() {
-    console.log(recognition.results);
+    console.log("this was said:" + recognition.results);
   }
   
 recognition.onerror = function(event) {
     diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
   }
 
-document.body.onclick = () => {
-    recognition.start();
-    console.log(window.document.body);
-    console.log("speech recognition started");
-  }
+  
+// document.body.onclick = () => {
+//     recognition.start();
+//     // console.log(window.document.body);
+//     console.log("speech recognition started");
+//   }
+
+startBtn.onclick = () => {
+  recognition.start();
+  // const speech = event.results[0][0].transcript;
+  document.getElementById("recordedText").innerHTML = "your speech is being recorded";
+  console.log("speech recognition has started");
+}
+
+recognition.addEventListener("result", (event) => {
+  const resultsToBePrinted = event.results[0][0].transcript;
+  diagnostic.textContent = `Results received: ${resultsToBePrinted}.`;
+  document.getElementById("recordedText").innerHTML = resultsToBePrinted;
+  // bg.style.backgroundColor = resultsToBePrinted;
+});
 
 
 endBtn.onclick = () => {
